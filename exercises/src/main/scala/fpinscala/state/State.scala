399270974aec0245eae4926f18787754ac08633e
ashabhasa <ashabhasa@gmail.com>
2016-04-09 19:17:02 +0200

    Implement map/flatmap for State case class.

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -132,27 +132,37 @@ object RNG {
     a => _map(rb) { b => f(a, b) }
   }
 
-
-  //  rng => {
-  //    val (a, rngA) = ra(rng)
-  //    val (b, rngB) = rb(rngA)
-  //    (f(a, b), rngB)
-  //  }
-
-
   def rollDie = map(nonNegativeLessThan(6))(_ + 1)
 }
 
+import State._
+
 case class State[S, +A](run: S => (A, S)) {
+
   def map[B](f: A => B): State[S, B] =
-    sys.error("todo")
+    State(s => {
+      val (a, s1) = run(s)
+      (f(a), s1)
+    })
+
+  def _map[B](f: A => B): State[S, B] = flatMap(a => unit(f(a)))
 
   def map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    sys.error("todo")
+    State(s => {
+      val (a, s1) = run(s)
+      val (b, s2) = sb.run(s1)
+      (f(a, b), s2)
+    })
+
+  def _map2[B, C](sb: State[S, B])(f: (A, B) => C): State[S, C] = flatMap(a => sb.map(b => f(a, b)))
 
   def flatMap[B](f: A => State[S, B]): State[S, B] =
-    sys.error("todo")
+    State(s => {
+      val (a, s1) = run(s)
+      f(a).run(s1)
+    })
 }
+
 
 sealed trait Input
 
@@ -164,6 +174,25 @@ case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object State {
   type Rand[A] = State[RNG, A]
+
+  def unit[S, A](a: A): State[S, A] = State(s => (a, s))
+
+  def sequence[S, A](fs: List[State[S, A]]): State[S, List[A]] = {
+    fs.foldLeft(unit(List[A]())){ (r,s) =>
+      s.map2(r) {(b,c) =>
+
+      }
+    }
+    ???
+  }
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
+    fs.foldLeft(unit(List[A]())) { (rl, r) =>
+      map2(r, rl) { (a, as) =>
+        a :: as
+      }
+    }
+  }
 
   def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
 }

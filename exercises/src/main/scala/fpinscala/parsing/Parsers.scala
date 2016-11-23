554f63fffa00a170433178bf71d3db5582fe014e
Arber Shabhasa <ashabhasa@gmail.com>
2016-11-23 20:49:19 +0100

    Begin implementing parsers.

--- exercises/src/main/scala/fpinscala/parsing/Parsers.scala
+++ exercises/src/main/scala/fpinscala/parsing/Parsers.scala
@@ -2,33 +2,48 @@ package fpinscala.parsing
 
 import language.higherKinds
 
-trait Parsers[Parser[+_]] { self => // so inner classes may call methods of trait
+trait Parsers[Parser[+ _]] {
+  self =>
+  // so inner classes may call methods of trait
+
+  def char(c: Char): Parser[Char]
+
+  implicit def string(s: String): Parser[String]
+
+  implicit def asStringParser[A](a: A)(implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
+
+  def or[A](s1: Parser[A], s2: Parser[A]): Parser[A]
+
+  def run[A](p: Parser[A])(input: String): Either[ParseError, A]
 
   case class ParserOps[A](p: Parser[A]) {
 
+    def |[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
 
+    def or[B >: A](p2: Parser[B]): Parser[B] = self.or(p, p2)
   }
 
   object Laws {
   }
+
 }
 
 case class Location(input: String, offset: Int = 0) {
 
-  lazy val line = input.slice(0,offset+1).count(_ == '\n') + 1
-  lazy val col = input.slice(0,offset+1).reverse.indexOf('\n')
+  lazy val line = input.slice(0, offset + 1).count(_ == '\n') + 1
+  lazy val col = input.slice(0, offset + 1).reverse.indexOf('\n')
 
   def toError(msg: String): ParseError =
     ParseError(List((this, msg)))
 
-  def advanceBy(n: Int) = copy(offset = offset+n)
+  def advanceBy(n: Int) = copy(offset = offset + n)
 
   /* Returns the line corresponding to this location */
-  def currentLine: String = 
-    if (input.length > 1) input.lines.drop(line-1).next
-    else ""
+  def currentLine: String =
+  if (input.length > 1) input.lines.drop(line - 1).next
+  else ""
 }
 
-case class ParseError(stack: List[(Location,String)] = List(),
+case class ParseError(stack: List[(Location, String)] = List(),
                       otherFailures: List[ParseError] = List()) {
 }
